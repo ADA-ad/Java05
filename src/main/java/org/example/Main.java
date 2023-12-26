0cc2a72daedf9eccd0b7a669d43d9e1aaa972fbf
ADA <tracyshi911@gmail.com>
2023-12-26 21:08:12 +0900

    add Mainクラスにlanguage injectionを使い、JSONコードを記述する

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String json = "{\"name\": \"noda\", \"age\": 66, \"Number\": [2, 4, 6]}";
+        System.out.println(json);
+        
+
+    }
+}
