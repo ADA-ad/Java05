fcd3dcf53fc51cbd59d9f0150767babde2545ae1
ADA <tracyshi911@gmail.com>
2023-12-26 21:17:03 +0900

    MainクラスのJSON形式を確認

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         String json = "{\"name\": \"noda\", \"age\": 66, \"Number\": [2, 4, 6]}";
         System.out.println(json);
-        
 
     }
 }
