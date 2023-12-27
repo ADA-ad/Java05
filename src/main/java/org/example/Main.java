9a071f147de9415c70f8b997fdbb7a07078a0ae2
ADA <tracyshi911@gmail.com>
2023-12-27 17:21:07 +0900

    add Mainクラス#7 json2のJSONデータを追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,26 @@ public class Main {
     public static void main(String[] args) {
         String json = "{\"name\": \"noda\", \"age\": 66, \"Number\": [2, 4, 6]}";
         System.out.println(json);
+        String json2 = "{\"total\":3,\n" +
+                       "\"cities\":[\n" +
+                       "{\n" +
+                       "\"Number\":1,\n" +
+                       "\"Prefecture\":\"Kanagawa\",\n" +
+                       "\"Capital\":\"Yokohama\"\n" +
+                       "},\n" +
+                       "{\n" +
+                       "\"Number\":2,\n" +
+                       "\"Prefecture\":\"Aichi\",\n" +
+                       "\"Capital\":\"Nagoya\"\n" +
+                       "},\n" +
+                       "{\n" +
+                       "\"Number\":3,\n" +
+                       "\"Prefecture\":\"Hyogo\",\n" +
+                       "\"Capital\":\"Kobe\"\n" +
+                       "}\n" +
+                       "]}";
+        System.out.println(json2);
 
     }
+
 }
